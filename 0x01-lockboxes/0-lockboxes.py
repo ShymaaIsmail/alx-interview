9f144972f3028a1969fb90d6493857e7b76710a2
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-04 09:32:21 +0300

    fix lock box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,15 +2,25 @@
 """ Lock Boxes Problem"""
 
 
+def recheck_locked_boxes(boxes, keys):
+    """ recheck_locked_boxes """
+    for k, box in boxes.items():
+        if k in keys:
+            for key in box:
+                keys.add(key)
+    return keys
+
+
 def canUnlockAll(boxes):
     """ canUnlockAll can unlock all box function"""
     keys = set()
     keys.add(0)
-    locked_boxes = [[]]
+    locked_boxes = {}
     for index, box in enumerate(boxes):
         if index in keys:
             for key in box:
                 keys.add(key)
         else:
-            locked_boxes.append(box)
+            locked_boxes[index] = box
+    recheck_locked_boxes(locked_boxes, keys)
     return len(keys) == len(boxes)
