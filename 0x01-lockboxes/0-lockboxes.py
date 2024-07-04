ad8dd45e5d20d59dc5fc5aa9534d16cb200db4b2
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-04 09:04:55 +0300

    still in progress

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python3
 """ Lock Boxes Problem"""
-
-def open_locked_boxes(boxes, keys ):
-    """open_locked_boxes"""
-    for index, box in enumerate(boxes):
-        print(f"index: {index}")
-        print(f"keys:  {keys}")
-        print(f"box:   {box}")
-        if index in keys:
-            for key in box:
-                keys.add(key)
-    return keys
-
 def canUnlockAll(boxes):
     """ canUnlockAll can unlock all box function"""
     keys = set()
     keys.add(0)
     locked_boxes = [[]]
     for index, box in enumerate(boxes):
-        print(f"kindex: {index}")
-        print(f"kkeys:  {keys}")
-        print(f"kbox:   {box}")
         if index in keys:
             for key in box:
                 keys.add(key)
