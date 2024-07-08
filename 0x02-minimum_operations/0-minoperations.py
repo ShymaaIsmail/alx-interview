75d3f98a05fa6d418813c9d0851e105c8e295ff6
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-08 11:02:51 +0300

    task0 in progress

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Solution for Minimum Operations Problem"""
+
+
+def minOperations(n: int) -> int:
+    """Return the minimum number of operations to reach n characters"""
+    operations_count = 0
+
+    return operations_count
