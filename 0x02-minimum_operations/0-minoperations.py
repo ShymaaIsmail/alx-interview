2804f7d564024d787ca28896d801019162752739
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-10 10:25:53 +0300

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,7 +5,14 @@
 def minOperations(n: int) -> int:
     """Return the minimum number of operations to reach n characters"""
     operations_count = 0
+    result = 0
     if 1 <= n <= 10**6:
-        operations_count = 0
+        while (result < n):
+            if (n + result) % 2 == 0:
+                result = result * 2 if result > 0 else result + 1
+            else:
+                result += 1
+            operations_count += 1
 
     return operations_count
+
