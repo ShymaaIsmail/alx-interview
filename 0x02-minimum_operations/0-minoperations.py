a5e95e1b19824c2ed1cdc5dd3a043d53d7ec4cf9
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-10 11:35:17 +0300

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,15 +9,12 @@ def minOperations(n: int) -> int:
     """Return the minimum number of operations to reach n characters"""
     if n <= 1:
         return 0
-    
     dp = [float('inf')] * (n + 1)
     dp[1] = 0
-    
     for i in range(2, n + 1):
         for j in range(1, int(math.sqrt(i)) + 1):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + (i // j))
                 if j != 1:
                     dp[i] = min(dp[i], dp[i // j] + j)
-    
     return dp[n]
