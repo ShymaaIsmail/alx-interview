19559e65920de664501e7df5193e5873b0a91d11
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-08-22 16:27:51 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 Module for the makeChange function.
@@ -18,12 +17,11 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    # Initialize dp array with infinity for all values except dp[0]
+    dp = [0] + [float('inf')] * total
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
